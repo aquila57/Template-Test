b911c8938ea5316f1c7fb3c75f20cd2ac97490f2
aquila57 <58740691+aquila57@users.noreply.github.com>
2019-12-17 10:07:04 -0600

    Add files via upload

---
+++ deinst.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+make -f tmplt.mak clean
+make -f gsltmplt.mak clean
+make -f dietmplt.mak clean
+make -f fibotmplt.mak clean
+make -f lfsr.mak clean
+make -f lfsr16.mak clean
+make -f sintmplt.mak clean
+make -f match.mak clean
+make -f randu.mak clean
+make -f randu16.mak clean
---
+++ inst.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+make -f tmplt.mak
+make -f gsltmplt.mak
+make -f dietmplt.mak
+make -f fibotmplt.mak
+make -f lfsr.mak
+make -f lfsr16.mak
+make -f sintmplt.mak
+make -f match.mak
+make -f randu.mak
+make -f randu16.mak
